64491895584ecf35645165c35cf736bc27730746
kkcl <2096648710@qq.com>
2020-04-12 11:36:31 +0800

    Site updated: 2020-04-12 11:36:30

--- js/src/utils.js
+++ js/src/utils.js
@@ -40,7 +40,7 @@ NexT.utils = NexT.$u = {
 
   lazyLoadPostsImages: function () {
     $('#posts').find('img').lazyload({
-      //placeholder: '/images/loading.gif',
+      // placeholder: '/images/loading.gif',
       effect: 'fadeIn',
       threshold : 0
     });
